ea478bdb56a9a904b8c96cd7211e74fa73fd0bd9
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-07-31 23:45:59 +0600

    init frontend app config

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,41 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+import tseslint from "typescript-eslint";
+import prettier from "eslint-config-prettier/flat";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  ...tseslint.configs.recommendedTypeChecked,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/consistent-type-imports": "error", // import type { X }
+      "@typescript-eslint/no-floating-promises": "error", // забытый await
+      "@typescript-eslint/no-misused-promises": "error", // async в onClick без обработки
+    },
+  },
+
+  prettier,
+]);
+
+export default eslintConfig;
