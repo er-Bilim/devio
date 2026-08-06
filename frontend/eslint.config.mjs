e8e1211089458bd20d0d15d62e2909daa49981fa
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-08-06 23:51:50 +0600

    chore(front): update deps and eslint config

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,8 +1,8 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
-import tseslint from "typescript-eslint";
-import prettier from "eslint-config-prettier/flat";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier/flat';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -11,10 +11,10 @@ const eslintConfig = defineConfig([
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
 
   {
@@ -25,13 +25,16 @@ const eslintConfig = defineConfig([
       },
     },
     rules: {
-      "@typescript-eslint/no-unused-vars": [
-        "error",
-        { argsIgnorePattern: "^_" },
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_' },
       ],
-      "@typescript-eslint/consistent-type-imports": "error", // import type { X }
-      "@typescript-eslint/no-floating-promises": "error", // забытый await
-      "@typescript-eslint/no-misused-promises": "error", // async в onClick без обработки
+      '@typescript-eslint/consistent-type-imports': 'error', // import type { X }
+      '@typescript-eslint/no-floating-promises': 'error', // забытый await
+      '@typescript-eslint/no-misused-promises': [
+        'error',
+        { checkVoidReturn: { attributes: false } },
+      ], // async в onClick без обработки
     },
   },
 
